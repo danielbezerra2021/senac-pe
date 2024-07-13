608af7c08039b1aa9090a6ad24cc4771680aa93f
Daniel bezerra <89395418+danielbezerra2021@users.noreply.github.com>
2024-07-13 09:14:36 -0300

    main.py

--- src/main.py
+++ src/main.py
@@ -16,8 +16,8 @@ Este é um projeto da disciplina de **Sistemas de Recomendação**
 **Alunos:**  
 Daniel Bezerra de Souza  
 Janilson Alves da Silva  
-Diogo Severino Antonio dos Santos
-Diogo Azevedo Pereira Dantas
+Diogo Severino Antonio dos Santos  
+Diogo Azevedo Pereira Dantas 
 """)
 
 @st.cache_data
